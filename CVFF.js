12ca6cb11bd70e744bcaf888bc467589fa9aa696
fferreyra91 <123647907+fferreyra91@users.noreply.github.com>
2023-01-26 18:28:53 -0300

    Add files via upload

---
+++ CVFF.js
@@ -0,0 +1,19 @@
+
+
+var div = document.getElementById("datosp");
+
+document.getElementById('datos').addEventListener('click', function (){
+    document.getElementById ('datosp').style.display = 'block'
+ })
+
+ var div = document.getElementById("detalle1");
+
+ document.getElementById('detalle').addEventListener('click', function (){
+     document.getElementById ('detalle1').style.display = 'block'
+  })
+
+var div = document.getElementById("detallelab2");
+
+  document.getElementById('detalle2').addEventListener('click', function (){
+      document.getElementById ('detallelab2').style.display = 'block'
+   })
